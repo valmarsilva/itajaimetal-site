06151ebdbea3f7bc4a5affeb15a23b78402f293e
valmarsilva <itapara30@gmail.com>
2026-05-12 12:54:12 -0300

    Add service worker for caching and fetch handling
    Implement service worker for caching files and handling fetch requests.

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,39 @@
 
+const CACHE_NAME = "itajaimetal-cache-v1";
+
+const ARQUIVOS_CACHE = [
+    "/",
+    "/index.html",
+    "/manifest.json",
+    "/logo.jpg"
+];
+
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(ARQUIVOS_CACHE);
+        })
+    );
+});
+
+self.addEventListener("activate", function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
